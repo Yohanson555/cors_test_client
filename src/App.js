efd7beab1e4fbc82082ab534ae41ac80410dec28
Yohanson555 <yohanson555@gmail.com>
2019-09-27 00:07:50 +0300

    misc

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ViewGallery from './components/ViewGallery';
+
+function App() {
+  return (
+    <div className="App" >
+      <ViewGallery />
+    </div>
+  );
+}
+
+export default App;
